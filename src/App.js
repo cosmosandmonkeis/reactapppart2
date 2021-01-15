f5ff78926ed1ecd1652cf89bd8104542295a2bca
David N <nguyen.david94579@gmail.com>
2021-01-15 10:36:02 -0800

    Added link to backend.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react'
 import Table from './Table'
 import Form from "./Form";
+import axios from "axios";
 
 class App extends Component {
     state = {
         characters: []
     };
 
+    makePostCall(character){
+        return axios.post('http://localhost:5000/users', character)
+            .then(function (response) {
+                console.log(response);
+                return (response.status === 200);
+            })
+            .catch(function (error) {
+                console.log(error);
+                return false;
+            });
+    }
+
     handleSubmit = character => {
-        this.setState({ characters: [...this.state.characters, character] })
-    };
+        this.makePostCall(character).then( callResult => {
+            if (callResult === true) {
+                this.setState({ characters: [...this.state.characters, character] });
+            }
+        });
+    }
 
     removeCharacter = index => {
         const { characters } = this.state
@@ -19,6 +36,18 @@ class App extends Component {
                 return i !== index
             }),
         })
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/users')
+            .then(res => {
+                const characters = res.data.users_list;
+                this.setState({ characters });
+            })
+            .catch(function (error) {
+                //Not handling the error. Just logging into the console.
+                console.log(error);
+            });
     }
 
     render() {
